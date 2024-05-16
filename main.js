a65c29121d09ade1ebe56e222f55ab9ef31066fe
Dulce Maria <dm9100546@gmail.com>
2024-05-15 21:56:03 -0300

    Criando a interface do projeto

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  app.whenReady().then(() => {
+    createWindow()
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
